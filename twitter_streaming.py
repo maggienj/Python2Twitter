1d7ea9b965fb627a5bc74e0c2248b5a3b37b54b2
Maggie <maggie.tech.nj@gmail.com>
2016-09-15 11:48:19 -0400

    Update twitter_streaming.py
    this is version-2. copied the exact code from local to github... as the local version had a few changes.

--- twitter_streaming.py
+++ twitter_streaming.py
@@ -1,17 +1,17 @@
 # Import the necessary package to process data in JSON format
-try:
-    import json
-except ImportError:
-    import simplejson as json
+# try:
+#    import json
+# except ImportError:
+import simplejson as json
 
 # Import the necessary methods from "twitter" library
 from twitter import Twitter, OAuth, TwitterHTTPError, TwitterStream
 
 # Variables that contains the user credentials to access Twitter API 
-ACCESS_TOKEN = 'YOUR ACCESS TOKEN"'
-ACCESS_SECRET = 'YOUR ACCESS TOKEN SECRET'
-CONSUMER_KEY = 'YOUR API KEY'
-CONSUMER_SECRET = 'ENTER YOUR API SECRET'
+ACCESS_TOKEN = 'rmvd-for-git"'
+ACCESS_SECRET = 'rmvd-for-git'
+CONSUMER_KEY = 'rmvd-for-git'
+CONSUMER_SECRET = 'rmvd-for-git'
 
 oauth = OAuth(ACCESS_TOKEN, ACCESS_SECRET, CONSUMER_KEY, CONSUMER_SECRET)
 
@@ -31,10 +31,12 @@ for tweet in iterator:
     # Twitter Python Tool wraps the data returned by Twitter 
     # as a TwitterDictResponse object.
     # We convert it back to the JSON format to print/score
-    print json.dumps(tweet)  
+    print ( json.dumps(tweet) )  
+    # print( json.dumps(evaluate(tweet), indent=4) )
     
     # The command below will do pretty printing for JSON data, try it out
-    # print json.dumps(tweet, indent=4)
+    # print ( json.dumps(tweet, indent=4) )
        
     if tweet_count <= 0:
         break 
+        
